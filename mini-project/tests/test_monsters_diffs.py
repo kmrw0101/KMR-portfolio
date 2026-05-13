2e710b39046f2addc895b71b4172383e7589f18e
Kerry Ritterbusch <kmrw0101@proton.me>
2026-05-13 12:14:31 -0500

    removed python tests to remove old LFS pointers

--- mini-project/tests/test_monsters_diffs.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b06bbd2658ba55d450208cbc56fe653f6564b2d97ba4669fd99dc2dae06042b
-size 855
--- mini-project/tests/test_monsters_perfect.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:baf9d1e0198a1ff05f69cbd12007e06f568c2723cf27aed79607fc08cad253fa
-size 858
